d7db2b0804f176166e73da33a149d5501b41467f
Anand <Anandthandapani11@gmail.com>
2024-11-25 21:52:33 +0530

    First push

---
+++ src/test/java/anand/Test1.java
@@ -0,0 +1,13 @@
+package anand;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void login() {
+		Reporter.log("hi",true);
+	}
+  
+       
+}
